7b139b29ffecf1f794f482e5437f8f4ec43b10c3
sh2dow <2311420+Sh2dow@users.noreply.github.com>
2024-10-10 17:29:37 +0300

    removed unused "Export" from SunInfo class

--- Nikki/Support.Carbon/Class/SunInfo.cs
+++ Nikki/Support.Carbon/Class/SunInfo.cs
@@ -10,9 +10,6 @@ using Nikki.Support.Shared.Parts.SunParts;
 using CoreExtensions.IO;
 using CoreExtensions.Conversions;
 
-using System;
-
-
 namespace Nikki.Support.Carbon.Class
 {
 	/// <summary>
@@ -256,26 +253,7 @@ namespace Nikki.Support.Carbon.Class
 				   $"BinKey: {this.BinKey:X8} | Game: {this.GameSTR}";
 		}
 
-        public void Export(SunInfo sunInfo, string filePath, bool serialized)
-        {
-            using var bw = new BinaryWriter(File.Open(filePath, FileMode.Create));
-    
-            // Check if serialized
-            if (serialized)
-            {
-                // Serialize the SunInfo instance
-                sunInfo.Serialize(bw);
-            }
-            else
-            {
-                // Non-serialized export (direct export of properties)
-                sunInfo.Assemble(bw);
-            }
-    
-            Console.Write($"SunInfo '{sunInfo.CollectionName}' successfully exported to {filePath}.", "Info");
-        }
-
-		#endregion
+        #endregion
 
 		#region Serialization
 
